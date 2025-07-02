31652f4d055bf5cb2b956a0bf1b7dbe8bda04bce
Aaron-GF <aarongarcia567@gmail.com>
2025-07-02 11:06:08 +0200

    feat: initialize jammming music player project with React, Vite, and routing
    - Added package.json with dependencies for React, React Router, and Vite.
    - Included SVG assets for Vite and React logos.
    - Created main application structure with App component and routing setup.
    - Developed screens for Home, Feed, Library, Player, Favorites, and Trending.
    - Implemented basic styling in App.css and index.css.
    - Configured Vite for development and build processes.

---
+++ src/screens/home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Library from './library';
+import Feed from './feed';
+import Trending from './trending';
+import Favorites from './favorites';
+import Player from './player';
+
+
+export default function Home() {
+    return(
+        <Router>
+            <Routes>
+                <Route path='/' element={<Library />} />
+                <Route path='/feed' element={<Feed />} />
+                <Route path='/player' element={<Player />} />
+                <Route path='/favorites' element={<Favorites />} />
+                <Route path='/trending' element={<Trending />} />
+            </Routes>
+        </Router>
+    )
+}
