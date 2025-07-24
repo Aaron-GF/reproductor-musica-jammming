5532ba5eed7ae89f7ca72a464dbdd6921546e97d
Aaron-GF <aarongarcia567@gmail.com>
2025-07-24 12:38:19 +0200

    fix: resolve import statement conflict for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import Home from './screens/home/Home.jsx';
-=======
-import Home from './screens/home/Home';
->>>>>>> 289a59013dd8ded86d9b40a4304ed8cf955b2f2a
 
 export default function App() {
   return (
