2331d555933e0cd4e8806be1285bbbdde28f257e
Aaron-GF <aarongarcia567@gmail.com>
2025-07-29 23:14:26 +0200

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from '@/screens/home/Home.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   return (
     <div>
       <Home />
+      <Analytics />
     </div>
   )
 }
